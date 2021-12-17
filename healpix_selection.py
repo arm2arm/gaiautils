e454ccd226ee9878c7783a6280ab1ce27a931ed8
Arman <arm2arm@gmail.com>
2021-12-17 23:39:44 +0100

    Update healpix_selection.py
    fixed missing where

--- healpix_selection.py
+++ healpix_selection.py
@@ -5,5 +5,5 @@ level=5
 sid_inf=hpnum*2**35*4**(12-level)
 sid_sup=(hpnum+1)*2**35*4**(12-level)
 
-query="SELECT gaia.source_id FROM gaiaedr3.gaia_source as gaia AND gaia.phot_g_mean_mag < 18"+\
+query="SELECT gaia.source_id FROM gaiaedr3.gaia_source as gaia WHERE gaia.phot_g_mean_mag < 18"+\
       " AND gaia.source_id BETWEEN {} AND {}".format(sid_inf,sid_sup)
